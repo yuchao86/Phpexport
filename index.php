227af8c499cc298b4869e7790cfd5a57e2317e73
Yu Chao <yuchao86@gmail.com>
2013-03-27 15:34:59 +0800

    test case php export

--- index.php
+++ index.php
@@ -1,26 +1,26 @@
 <?php
 include 'Phpexport.php';
 
-$columns = array("第一","第二列","第三列");
+$columns = array("第一列","第二列","第三列");
 
 $data = array(
   array("1","2","3"),
   array("2","3","4"),
-  array("english","英语","@@#@"),
+  array("english","英语","余超"),
   array("+","-","*")
 );
 
 
 $phpexport = new Phpexport($data);
-$phpexport->clearCache();
+//$phpexport->clearCache();
 $phpexport->setColumns($columns);
 $phpexport->setNotes("YuChao");
 
-$phpexport->setFilename("yuchao_2013_03_28");
+$phpexport->setFilename("yuchao_20130328");
 
-//$phpexport->getExportHeader('csv');
+$phpexport->getExportHeader('csv');
 
-//$phpexport->OutputCSV();
-$phpexport->extOutputCSV();
+$phpexport->OutputCSV();
+//$phpexport->extOutputCSV();
 
 ?>
