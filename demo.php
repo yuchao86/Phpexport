310e0016f6a961529ba22addaaebac66e3315da0
Yu Chao <yuchao86@gmail.com>
2013-03-27 15:37:00 +0800

    add extension information

--- demo.php
+++ demo.php
@@ -1,4 +1,28 @@
 <?php
+
+/**
+ * Phpexport - Open source export data
+ *
+ * @link http://github.com/yuchao86/phpexport.git
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ * @category Phpexport
+ * @package Phpexport
+ */
+
+/**
+ * CSV export
+ *
+ * When rendered using the default settings, a CSV report has the following characteristics:
+ * The first record contains headers for all the columns in the report.
+ * All rows have the same number of columns.
+ * The default field delimiter string is a comma (,).
+ * Formatting and layout are ignored.
+ *
+ * @package Phpexport
+ * @subpackage DataTable
+ */
+ 
 header("Content-Type: application/vnd.ms-execl");
 header("Content-Disposition: attachment; filename=myExcel.xls");
 header("Pragma: no-cache");
